128f12146470ba18cb3ba3d71d1c414eb14847c9
Ahsanprantik <prantik.ahsan00@gmail.com>
2023-10-31 16:33:58 +0600

    Add files via upload

---
+++ Android_Apps/Stopwatch_timer/Stopwatch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+#define lim 1e8
+
+int main()
+{
+	int init;
+	printf("Press ENTER to start....");
+	getchar();
+	puts("Time:\n");
+
+	init = clock();
+	while(1)    printf("\r\t %07.4f", (double)(clock()-init)/(CLOCKS_PER_SEC));
+
+	printf("\nThank you for using me.\nR. Giskard.");
+
+	return 0;
+}
---
+++ Android_Apps/Stopwatch_timer/Timer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+
+#define eps 1e-15
+
+int main(void)
+{
+	long double init, s;
+	int m, h;
+	char nouse;
+
+	printf("Set timer(sec): ");
+	scanf("%d %c %d %c %lf", &h, &nouse, &m, &nouse, &s);
+
+	puts("Time remaining:");
+	init = clock() + (s + 60 * (m + 60 * h)) * CLOCKS_PER_SEC;
+	printf("%f", init);
+    printf("\r\t %02d : %02d : %.2f", h, m, s);
+	while(clock()<=init) {
+        h = (double)(init-clock())/(CLOCKS_PER_SEC*3600.0);
+        m = (double)(init-clock())/(CLOCKS_PER_SEC*60.0);
+        s = (double)(init-clock())/(CLOCKS_PER_SEC);
+		printf("\r\t %02d : %02d : %.2f", h, m, s);
+	}
+
+	printf("Time over\n");
+	printf("\nThank you for using me.\nR. Giskard.");
+	getchar();
+
+	return 0;
+}
